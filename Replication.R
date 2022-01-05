828c043d3a6ede8ed9c0a6c8e7e9d794b92f512c
Hannah <hannah.haegele@t-online.de>
2022-01-05 12:26:50 +0100

    Copy of Exploration_v6 from Masterthesis R-Project.

---
+++ Replication.R
@@ -0,0 +1,363 @@
+#16/12/21 interpolation, finalizing dataset 
+#17/12/21 fixed window regressions -> estimates, variances -> current version 
+#18/12/21 import price inflation neu (plus alternative estimation); seasonal adjustment angefangen 
+#20/12/21 v3 
+#21/12/21 v4, mistake in sqrt(diag(vcov(reg)))) -> diag(vcov(reg))
+#22/12/21 fixed mistake with data in rolling window (used data1 previously instead of data), covariance matrices, 
+#         fixed mistake with lagged values of lagged yoy_inflation (both in dataset creation and h function, H Jacobian)
+
+#differences v4: constrained least squares using ConsReg instead of non-constrained least squares 
+#                full Q matrix (covariance averages) instead of diagonal Q matrix 
+#differences v5: function h now includes lagged yoy_inflation (by one quarter)
+#differences v6: tried to implement the first constraint in the filter (quadratic programming problem)
+
+
+#preamble ####
+rm(list = ls())
+options(scipen = 999)
+
+library(tidyverse)
+library(readr)
+library(corrplot)
+library(dplyr)
+library(magrittr)
+library(data.table)
+library(here)
+library(seasonal) #used for seasonsal adjustment
+library(ggplot2)
+library(ConsReg)
+library(quadprog)
+library(cowplot)
+
+
+#read in data ####
+
+#covid dummy
+covid <- as.data.frame(read_csv(here('0_Data/phillips_corona.csv'))) %>%
+  select(c(covid,date))
+
+#data unemployment (sa)
+unemployment <- read_csv(here('0_Data/UNRATE.csv')) %>%
+  rename(unemp = UNRATE) 
+#inflation_expectation$expect <- as.numeric(expect)
+as.data.frame(unemployment)
+
+#data NAIRU (not sa)
+NAIRU <- read_csv(here('0_Data/NROU.csv')) %>%
+  rename(NAIRU = NROU) 
+#NAIRU$NAIRU <- as.numeric(NAIRU$NAIRU)
+as.data.frame(NAIRU)
+
+#data inflation expectation (not sa)
+inflation_expectation <- read_csv(here('0_Data/EXPINF10YR.csv')) %>%
+  rename(expect = EXPINF10YR, ) 
+#inflation_expectation$expect <- as.numeric(expect)
+as.data.frame(inflation_expectation)
+
+#data import price deflator (sa)
+importpricedeflator <- as.data.frame(read_csv(here('0_Data/A255RD3Q086SBEA.csv'))) %>%
+  rename(IMPdef = A255RD3Q086SBEA) 
+
+#data cpi (sa)
+cpi <- read_csv(here('0_Data/CPALTT01USM661S.csv')) %>%
+  rename(cpi = CPALTT01USM661S) 
+as.data.frame(cpi)
+
+#testing inflation expectation ts for seasonal adjustment ####
+
+inflationexpectation0<-ts(inflation_expectation[,-1],frequency=12,start=c(1982,1))
+inflationexpectation0.seas<-seas(inflationexpectation0)
+qs(inflationexpectation0.seas)
+
+#there is no evidence of seasonality in the original and hence the time series is not adjusted
+
+#inflation ####
+cpi$monthly_inflation <- NA
+for (i in 2:nrow(cpi)) {
+  cpi$monthly_inflation[i] <- ((cpi$cpi[i]-cpi$cpi[i-1])/cpi$cpi[i-1])*100
+}
+
+cpi$annualized_inflation <- cpi$monthly_inflation*12
+
+cpi$yoy_inflation <- NA
+for (i in 13:nrow(cpi)) {
+  cpi$yoy_inflation[i] <- ((cpi$cpi[i]-cpi$cpi[i-12])/cpi$cpi[i-12])*100
+}
+
+#import price inflation ####
+
+#interpolation for monthly import deflator data 
+monthly = seq(importpricedeflator$DATE[1], tail(importpricedeflator$DATE,1), by="month")
+importpricedeflator2 <- data.frame(DATE=monthly, spline(importpricedeflator, method="fmm",xout = monthly)$y) 
+importpricedeflator <- merge(importpricedeflator, importpricedeflator2, by="DATE", all=TRUE) %>%
+  rename(IMPdef_monthly = spline.importpricedeflator..method....fmm...xout...monthly..y)
+
+#monthly growth rates (in percentage terms)
+importpricedeflator$monthly_importpriceinflation <- NA
+for (i in 2:nrow(importpricedeflator)) {
+  importpricedeflator$monthly_importpriceinflation[i] <- ((importpricedeflator$IMPdef_monthly[i]-importpricedeflator$IMPdef_monthly[i-1])/importpricedeflator$IMPdef_monthly[i-1])*100
+}
+
+#annualized growth rates 
+importpricedeflator$importpriceinflation <- importpricedeflator$monthly_importpriceinflation*12
+
+drop.cols <- c('IMPdef','IMPdef_monthly')
+importpricedeflator <- importpricedeflator %>% select(-one_of(drop.cols))
+
+
+
+#alternative for import price inflation ####
+#yields slighlty different results (but negative theta)
+
+
+#quarterly growth rates
+# importpricedeflator$quarterly_importpriceinflation <- NA
+# for (i in 2:nrow(importpricedeflator)) {
+#   importpricedeflator$quarterly_importpriceinflation[i] <- (importpricedeflator$IMPdef[i]-importpricedeflator$IMPdef[i-1])/importpricedeflator$IMPdef[i-1]*100
+# }
+# 
+# #annualized growth rates
+# importpricedeflator$importpriceinflation1 <- importpricedeflator$quarterly_importpriceinflation*4
+# 
+# drop.cols <- c('IMPdef','quarterly_importpriceinflation')
+# importpricedeflator <- importpricedeflator %>% select(-one_of(drop.cols))
+# 
+# #interpolation for monthly growth rates
+# monthly = seq(importpricedeflator$DATE[1], tail(importpricedeflator$DATE,1), by="month")
+# importpricedeflator2 <- data.frame(DATE=monthly, spline(importpricedeflator, method="fmm",xout = monthly)$y)
+# importpricedeflator <- merge(importpricedeflator, importpricedeflator2, by="DATE", all=TRUE) %>%
+#   rename(importpriceinflation = spline.importpricedeflator..method....fmm...xout...monthly..y)
+# 
+
+
+#merge and transform data ####
+
+data <- merge(unemployment,inflation_expectation,by.x = "DATE", by.y = "DATE", all.x = TRUE)  %>%
+  merge(importpricedeflator,by.x = "DATE", by.y = "DATE", all.x = TRUE) %>%
+  merge(NAIRU,by.x = "DATE", by.y = "DATE", all.x = TRUE) %>%
+  merge(cpi,by.x = "DATE", by.y = "DATE", all.x = TRUE) %>%
+  merge(covid,by.x = "DATE", by.y = "date", all.x = TRUE) %>%
+  mutate(expect_yoy = expect-lag(yoy_inflation,3L)) %>%
+  mutate(relativeimportpriceinflation = importpriceinflation-annualized_inflation) %>%
+  rename(date = DATE) %>%
+  relocate(covid, .after = date) %>%
+  select(c(date,covid,unemp,expect,importpriceinflation,NAIRU,annualized_inflation,yoy_inflation,expect_yoy,relativeimportpriceinflation))
+
+
+#subset of sample that contains data for all variables (expected inflation)
+data1 <- filter(data,date > "1981-12-01" & date < "2021-08-01")
+data1$t <- 1:nrow(data1) 
+data1 <- relocate(data1, t) 
+
+data1 <- as_tibble(data1)
+write_csv(data1, "model_input_data.csv")
+
+data2 <- filter(data,date > "1981-09-01" & date < "2021-08-01")
+data2$t <- 1:nrow(data2) 
+data2 <- relocate(data2, t) 
+
+
+#scatter-plot COVID ####
+
+# ggplot(data=data, aes(x=unemp, y=annualized_inflation))+
+#   geom_point()
+# 
+# 
+# png(filename = here("1_Plots/Correlation_Covid.png") , height=350, width=350)
+# ggplot(data=data %>% filter(covid==1), aes(x=unemp, y=annualized_inflation))+
+#   geom_point()
+# dev.off()
+
+
+
+#linear regression ####
+#sample: 1982-2021
+# reg <- lm(annualized_inflation~unemp+relativeimportpriceinflation+expect_yoy,data=data1)
+# summary(reg)
+# vcov(reg)
+# 
+# # 
+# # #regression with unemployment gap, using empirical NAIRU
+# # unempgap <- data1$unemp-data1$NAIRU
+# # reg <- lm(annualized_inflation~unempgap+relativeimportpriceinflation+expect_yoy,data=data1)
+# # summary(reg)
+# # 
+# reg <- ConsReg(annualized_inflation~unemp+relativeimportpriceinflation+expect_yoy,data=data1,
+#                constraints ='unemp < 0,relativeimportpriceinflation > 0,expect_yoy > 0',optimizer='mcmc',family='gaussian',ini.pars.coef = c(3.5,-0.5,0.1,0.2))
+# summary(reg)
+
+
+#rolling window regressions ####
+#if we have data on the full year 2021, then we would have 31 full rolling windows 
+#df=number observations - number of parameters (including intercept)
+
+for (i in 1:31) {
+  data <- filter(data1, t>=1+(i-1)*12 & t<=120+(i-1)*12) 
+  reg <- ConsReg(annualized_inflation~unemp+relativeimportpriceinflation+expect_yoy,data=data,
+                 constraints ='unemp <= 0,relativeimportpriceinflation >= 0,expect_yoy >= 0',optimizer='mcmc',family='gaussian',ini.pars.coef = c(3.5,-0.5,0.1,0.2))
+  
+  X <- cbind(rep(1,nrow(data)),data$unemp,data$relativeimportpriceinflation,data$expect_yoy)
+  
+  as.vector(assign(paste0("coefficients_", i), summary(reg)$coeff[1:4]))
+  as.vector(assign(paste0("variances_coefficients_", i), (summary(reg)$coeff[5:8])^2))
+  
+  assign(paste0("variance_estimate_", i), sum(reg$residuals^2)/(nrow(data)-4))
+  
+  #variance-covariance matrices = sigma2*(X'X)^-1
+  assign(paste0("covariance_matrix_", i), (sum(reg$residuals^2)/(nrow(data)-4)) * solve(crossprod(X)))
+}
+
+#average of all coefficients, their variances and the variance of the estimate (i.e. of the residuals/regression)
+coefficients <- do.call(rbind, lapply( paste0("coefficients_", 1:31) , get) )
+variances_coefficients <- do.call(rbind, lapply( paste0("variances_coefficients_", 1:31) , get) )
+
+coefficients_0 <- as.vector(colMeans(coefficients))[2:4]
+variances_coefficients_0 <- as.vector(colMeans(variances_coefficients))[2:4]
+
+variance_estimate <- do.call(rbind, lapply( paste0("variance_estimate_", 1:31) , get) )
+variance_estimate_0 <- as.vector(colMeans(variance_estimate))
+
+variance_NAIRU <- var(data1$NAIRU,na.rm = TRUE)
+
+#average of all variance-covariance matrices 
+covariances_0 <- do.call(cbind, lapply( paste0("covariance_matrix_", 1:i) , get))
+covariances_0  <- array(covariances_0, dim=c(4,4,i))
+covariances_0 <- apply(covariances_0 , c(1, 2), mean, na.rm = TRUE)
+
+#print output
+variances_coefficients_0
+variance_estimate_0 
+variance_NAIRU
+covariances_0
+
+
+#EKF recursions ####
+
+#initialization (values, vectors, matrices) 
+s = 15 
+rho = 0.9 
+
+x_prior <- matrix(NA, nrow = 4, ncol = nrow(data2)-3)
+x_post <- matrix(NA, nrow = 4, ncol = nrow(data2)-3+1)
+x_post[,1] <- c(0,coefficients_0)
+#w <- c(s*variance_NAIRU,variances_coefficients_0)
+
+Q <- covariances_0
+Q[1,] <- c(variance_NAIRU*s,0,0,0)
+Q[,1] <- c(variance_NAIRU*s,0,0,0)
+
+R <- variance_estimate_0 
+
+P_prior <- matrix(0, nrow = 4, ncol = 4)
+P_post <- matrix(0, nrow = 4, ncol = 4)
+
+F <- diag(c(rho,1,1,1), 4, 4)
+
+h <- function(i) {
+  (x_prior[2,i])*(x_prior[1,i]) + x_prior[3,i]*data2$relativeimportpriceinflation[i+3] + x_prior[4,i]*data2$expect[i+3] + (1-x_prior[4,i])*data2$yoy_inflation[i]
+}
+
+H <- function(i) {
+  t(c((x_prior[2,i]), (x_prior[1,i]), data2$relativeimportpriceinflation[i+3], (data2$expect[i+3]-data2$yoy_inflation[i])))
+}
+
+#Extended Kalman filter recursions
+for (i in 1:(nrow(data2)-3)) {
+  
+  #prediction step 
+  x_prior[,i] <- F %*% x_post[,i]
+  P_prior <- F %*% tcrossprod(P_post,F) + Q 
+  
+  #update step 
+  y <- data2$annualized_inflation[i+3]-h(i)
+  S <- H(i) %*% tcrossprod(P_prior,H(i)) + R
+  S_inv <- solve(S)
+  K <- tcrossprod(P_prior,H(i)) %*% S_inv
+  
+  # For tracking progress
+  if (y > variance_estimate_0) {
+    print(paste0("the estimated residual with value ",y," is larger than average initial shock variance at iteration ",data2$date[i]))
+    #y <- variance_estimate_0
+  }
+  
+  
+  x_post[,i+1] <- x_prior[,i] + K %*% y
+  P_post <- (diag(1,4,4) - K %*% H(i)) %*% P_prior
+  
+  if (x_post[2,i+1]<0 | x_post[3,i+1]<0 | x_post[4,i+1]<0 | x_post[4,i+1]>1) {
+    
+    #constraints for the quadratic optimization problem 
+    # lower-bounds 
+    A.lbs <- rbind(c( 1, 0, 0, 0),
+                   c( 0, 1, 0, 0),
+                   c( 0, 0, 1, 0),
+                   c( 0, 0, 0, 1))
+    b.lbs <- c(-1000, 0, 0, 0)
+    
+    # upper-bounds on variables
+    A.ubs <- rbind(c( -1, 0, 0, 0),
+                   c( 0, -1, 0, 0),
+                   c( 0, 0, -1, 0),
+                   c( 0, 0, 0, -1))
+    b.ubs <-  c(-1000, -1000, -1000, -1)
+    
+    Amat = t(rbind(A.lbs, A.ubs))
+    bvec = c(b.lbs, b.ubs)
+    
+    #quadratic optimisation model 
+    Dmat <- matrix(0,4,4)
+    
+    P_post_inv <- solve(P_post)
+    diag(Dmat) <- diag(P_post_inv)*2
+    
+    Dmat[lower.tri(Dmat)] <- c(P_post_inv[1,2]+P_post_inv[2,1], P_post_inv[1,3]+P_post_inv[3,1],P_post_inv[1,4]+P_post_inv[4,1],P_post_inv[2,3]+P_post_inv[3,2],P_post_inv[2,4]+P_post_inv[4,2],P_post_inv[3,4]+P_post_inv[4,3])
+    Dmat[upper.tri(Dmat)] <- c(P_post_inv[1,2]+P_post_inv[2,1], P_post_inv[1,3]+P_post_inv[3,1],P_post_inv[2,3]+P_post_inv[3,2],P_post_inv[4,1]+P_post_inv[1,4],P_post_inv[2,4]+P_post_inv[4,2],P_post_inv[3,4]+P_post_inv[4,3])
+    
+    a <- t(-x_post[,i+1]) %*% P_post_inv[,1] + P_post_inv[1,2] * (-x_post[2,i+1]) + P_post_inv[1,3] * (-x_post[3,i+1]) + P_post_inv[1,4] * (-x_post[4,i+1])
+    
+    b <- t(-x_post[,i+1]) %*% P_post_inv[,2] + P_post_inv[2,1] * (-x_post[1,i+1]) + P_post_inv[2,3] * (-x_post[3,i+1]) + P_post_inv[2,4] * (-x_post[4,i+1])
+    
+    c <- t(-x_post[,i+1]) %*% P_post_inv[,3] + P_post_inv[3,1] * (-x_post[1,i+1]) + P_post_inv[3,2] * (-x_post[2,i+1]) + P_post_inv[3,4] * (-x_post[4,i+1])
+    
+    d <- t(-x_post[,i+1]) %*% P_post_inv[,4] + P_post_inv[4,1] * (-x_post[1,i+1]) + P_post_inv[4,2] * (-x_post[2,i+1]) + P_post_inv[4,3] * (-x_post[3,i+1])
+    
+    dvec <- c(-a,-b,-c,-d)
+    
+    solve.QP(Dmat,dvec,Amat,bvec=bvec)
+    
+    x_post[,i+1] <- solve.QP(Dmat,dvec,Amat,bvec=bvec)$solution
+    
+  }
+  
+  
+}
+
+#plotting results ####
+
+#simple plot
+par(mfrow=c(2,2))
+plot(x_post[1,],type="line",sub ="unemployment gap",xlab = "",ylab = "")
+plot(x_post[2,],type="line",sub ="kappa",xlab = "",ylab = "")
+plot(x_post[3,],type="line",sub ="gamma",xlab = "",ylab = "")
+plot(x_post[4,],type="line",sub ="theta",xlab = "",ylab = "")
+
+#using ggplot
+#convert matrix to a wide data frame, delete starting values (first row)
+x_post_data <- as.data.frame(t(x_post))
+x_post_data <- x_post_data[-1,]
+x_post_data$date <- c(as.character(data2$date[-(1:3)]))
+x_post_data$date <- as.POSIXct(x_post_data$date, format="%Y-%m-%d")
+
+colnames(x_post_data) <- c("unemploymentgap","kappa","gamma","theta","date")
+
+
+a <- ggplot(data = x_post_data,mapping = aes(x = date, y = unemploymentgap,group = 1)) + geom_line()
+b <- ggplot(data = x_post_data,mapping = aes(x = date, y = kappa,group = 1)) + geom_line()
+c <- ggplot(data = x_post_data,mapping = aes(x = date, y = gamma,group = 1)) + geom_line()
+d <- ggplot(data = x_post_data,mapping = aes(x = date, y = theta,group = 1)) + geom_line()
+
+plot_grid(a, b, c, d, labels = "AUTO")
+
+
+
+
